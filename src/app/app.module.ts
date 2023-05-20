b2fa2c719a5b84d2f6c1fc66e65ad540be2cebfb
Kamil <tomala.kamil@hotmail.com>
2023-05-20 23:54:00 +0200

    Add Signals component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {SignalsComponent} from "./signals/signals.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignalsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'signals', component: SignalsComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/signals/signals.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: 'signals.component.html'
+})
+
+export class SignalsComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
