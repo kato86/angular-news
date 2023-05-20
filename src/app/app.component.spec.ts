d83a5086d32277532dd7a5f11535c0e359a0cbb1
Kamil <tomala.kamil@hotmail.com>
2023-05-21 00:22:13 +0200

    fix: add title to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-news-2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-news-2');
+    expect(app.title).toEqual('angular-news');
   });
 
   it('should render title', () => {
