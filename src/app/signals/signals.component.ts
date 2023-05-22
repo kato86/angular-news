65ddf48e39e7efd5e717a816f86df1f497081c1f
Kamil <tomala.kamil@hotmail.com>
2023-05-22 23:26:04 +0200

    feat: signals - counter

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal, WritableSignal} from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -6,9 +6,27 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class SignalsComponent implements OnInit {
+
+  count: WritableSignal<number> = signal(0);
+
   constructor() {
   }
 
   ngOnInit() {
+
+  }
+
+  onIncrementCount() {
+    console.log('Increment +');
+    this.count.update(value => value + 1);
+  }
+
+  onDecrementCount() {
+    console.log('Decrement -');
+    this.count.update(value => value - 1);
+  }
+
+  onResetCount() {
+    this.count.set(0);
   }
 }
